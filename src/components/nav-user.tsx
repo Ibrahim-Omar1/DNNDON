32db89a2ef89e70025ab85550eed0ebeb237845a
Ibrahim <ibrahim.omar@travolic.com>
2025-02-19 18:47:29 +0200

    Remove user prop from NavUser component

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -30,15 +30,7 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+export function NavUser() {
   const { isMobile } = useSidebar()
 
   return (
@@ -51,7 +43,7 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src="https://avatars.githubusercontent.com/u/10353856?v=4" alt={user.name} />
+                <AvatarImage src="https://avatars.githubusercontent.com/u/10353856?v=4" alt="Ibrahim Omar" />
                 <AvatarFallback className="rounded-lg">IO</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
@@ -70,7 +62,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src="https://avatars.githubusercontent.com/u/10353856?v=4" alt={user.name} />
+                  <AvatarImage src="https://avatars.githubusercontent.com/u/10353856?v=4" alt="Ibrahim Omar" />
                   <AvatarFallback className="rounded-lg">IO</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
